1d089de50e8202d92fe7953576fb3e1e678c5ba4
xuyanpeng <327588577@qq.com>
2019-12-04 10:00:37 +0800

    创建项目

---
+++ lib/models/profile.dart
@@ -0,0 +1,19 @@
+import 'package:json_annotation/json_annotation.dart';
+import "user.dart";
+import "cacheConfig.dart";
+part 'profile.g.dart';
+
+@JsonSerializable()
+class Profile {
+    Profile();
+
+    User user;
+    String token;
+    num theme;
+    CacheConfig cache;
+    String lastLogin;//最后一次注销登录的用户名
+    String locale;
+    
+    factory Profile.fromJson(Map<String,dynamic> json) => _$ProfileFromJson(json);
+    Map<String, dynamic> toJson() => _$ProfileToJson(this);
+}
